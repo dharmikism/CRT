5b23dec1dbdf219fc7a17b7e4cecf898480e5aba
dharmikism <dharmikism@gmail.com>
2026-02-16 09:39:52 +0530

    21 push

---
+++ IV th sem/Python/Practice/M02/S03/PS03.py
@@ -0,0 +1,29 @@
+'''
+a,d=map(int,input().split())
+for i in range(1,11):
+    print(a+(i-1)*d,end=" ")
+
+a,r=map(int,input().split())
+for i in range(1,11):
+    print(a*(r**(i-1)),end=" ")
+
+a=0
+b=1
+n=int(input())
+print(a,b,end=" ")
+for i in range(2,n):
+    c=a+b
+    print(c,end=" ")
+    a=b 
+    b=c 
+'''
+
+a,b,c=map(int,input().split())
+if b-a==c-b:
+    print("AP")
+elif b/a==c/b:
+    print("GP")
+elif a+b==c:
+    print("FIB")
+elif a**2+b**2==c**2:
+    print("Pythagorean Triplet")

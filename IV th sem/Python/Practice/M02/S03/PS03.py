e325a8f991db2cb191292abbe67341740f03e91d
dharmikism <dharmikism@gmail.com>
2026-02-21 14:27:12 +0530

    idk what im doinng

--- IV th sem/Python/Practice/M02/S03/PS03.py
+++ IV th sem/Python/Practice/M02/S03/PS03.py
@@ -57,7 +57,6 @@ for i in range(2,n):
     c=l[i-2]+l[i-1]
     l.append(c)
 print(*l)
-'''
 
 # Problem 6: Factorial Calculation
 # Problem Statement: Calculate the factorial of a given number n using recursion
@@ -74,3 +73,32 @@ def fact(n):
 
 n=int(input())
 print(fact(n))
+
+def strong(n):
+    s=0
+    temp=n
+    while temp>0:
+        f=1
+        for i in range(1,temp%10+1):
+            f*=i
+        s+=f
+        temp=temp//10
+    if s==n:
+        return "The number is a Strong number."
+    else:
+        return "The number is not a Strong number."
+n=int(input())
+print(strong(n))
+
+def perfect(n):
+    s=0
+    for i in range(1,n):
+        if n%i==0:
+            s+=i
+    if s==n:
+        return "The number is a Perfect number."
+    else:
+        return "The number is not a Perfect number."
+n=int(input())
+print(perfect(n))
+'''

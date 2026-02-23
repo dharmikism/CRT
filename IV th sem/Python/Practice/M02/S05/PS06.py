d50ee67d75f1582f3585d23c58df88ed94d442b1
dharmikism <dharmikism@gmail.com>
2026-02-23 09:49:06 +0530

    but im just a boy

---
+++ IV th sem/Python/Practice/M02/S05/PS06.py
@@ -0,0 +1,18 @@
+'''
+li=[1,2,3,4,5]
+output=[1,4,9,16,25]
+
+li=list(map(int,input().split()))
+print(*list(map(lambda x:x**2,li)))
+
+input = [1,2,3,4,5]
+output-[2,4]
+
+li=list(map(int,input().split()))
+print(*list(filter(lambda x:x%2==0,li)))
+
+input=['d','h','a','r','m','i','k']
+output= dharmik
+'''
+li=list(input().split())
+print("".join(li))
